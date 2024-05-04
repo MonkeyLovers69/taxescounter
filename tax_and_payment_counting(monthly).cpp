bfee48d3b3060cc23309777918febf9653c99eb4
MonkeyLovers69 <137757068+MonkeyLovers69@users.noreply.github.com>
2024-05-04 17:59:13 +0500

    Add files via upload

---
+++ tax_and_payment_counting(monthly).cpp
@@ -0,0 +1,31 @@
+﻿#include <iostream>
+using namespace std;
+int rooms;
+class taxes
+{
+public:
+	void counter() {
+		cout << "\ntax percent : 6%";
+		cout << "\ncost per room : $" << tax_to_pay_for;
+		cout << "\ntotal payment for rooms : $" << tax_to_pay_for;
+		cout << "\ntaxes : $" << tax;
+		cout << "\nyou have to pay in total : $" << total;
+	}
+
+private:
+	double tax_percent{ 0.06 };
+	double paymentperRoom{ 30 };
+	double tax_to_pay_for{ paymentperRoom * rooms };
+	double tax{ (tax_to_pay_for * tax_percent) };
+	double total{ (rooms * paymentperRoom) + tax };
+};
+
+int main()
+{
+	cout << "Welcome to our tax counting app to make your life easier! ";
+	cout << "\nHow many rooms you need to pay for?\n";
+	cout << "Rooms : "; 
+	cin >> rooms;
+	taxes objecttouse{};
+	objecttouse.counter();
+}
