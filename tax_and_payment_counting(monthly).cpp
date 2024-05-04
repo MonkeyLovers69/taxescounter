b6a74cd7922475b336ebf00d97ff95a1c142e5e2
MonkeyLovers69 <137757068+MonkeyLovers69@users.noreply.github.com>
2024-05-04 18:18:11 +0500

    demo tax_and_payment_counting(monthly).cpp
    i should upgrade it one day for poor american people

--- tax_and_payment_counting(monthly).cpp
+++ tax_and_payment_counting(monthly).cpp
@@ -1,23 +1,25 @@
-﻿#include <iostream>
+#include <iostream>
 using namespace std;
 int rooms;
 class taxes
 {
 public:
 	void counter() {
-		cout << "\ntax percent : 6%";
+		cout << "\ntax percent : 6%"; 
 		cout << "\ncost per room : $" << tax_to_pay_for;
 		cout << "\ntotal payment for rooms : $" << tax_to_pay_for;
 		cout << "\ntaxes : $" << tax;
+		cout<<"\n=========================================================="
 		cout << "\nyou have to pay in total : $" << total;
+		cout << "IMPORTANT:valid for 30 days";
 	}
 
 private:
-	double tax_percent{ 0.06 };
-	double paymentperRoom{ 30 };
-	double tax_to_pay_for{ paymentperRoom * rooms };
-	double tax{ (tax_to_pay_for * tax_percent) };
-	double total{ (rooms * paymentperRoom) + tax };
+	double tax_percent{ 0.06 }; //to do math with percents
+	double paymentperRoom{ 30 }; 
+	double tax_to_pay_for{ paymentperRoom * rooms }; //counting what tax comes from
+	double tax{ (tax_to_pay_for * tax_percent) };//tax itself
+	double total{ (rooms * paymentperRoom) + tax };//just total for everything i mean c'mon who would've guessed lol .!.
 };
 
 int main()
